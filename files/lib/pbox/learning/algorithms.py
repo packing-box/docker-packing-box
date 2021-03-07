25fac7b167ead9504e2f9fc73935681c81af9743
dhondta <alexandre.dhondt@gmail.com>
2021-03-07 21:50:32 +0100

    Improved pbox.learning

--- files/lib/pbox/learning/algorithms.py
+++ files/lib/pbox/learning/algorithms.py
@@ -1,7 +1,6 @@
 # -*- coding: UTF-8 -*-
 import pandas as pd
 from sklearn.ensemble import AdaBoostClassifier as ABC, RandomForestClassifier as RFC
-from sklearn.model_selection import GridSearchCV
 from sklearn.naive_bayes import BernoulliNB as BNB, GaussianNB as GNB, MultinomialNB as MNB
 from sklearn.neighbors import KNeighborsClassifier as KNC
 from sklearn.neural_network import MLPClassifier as MLPC
@@ -12,28 +11,8 @@ from weka.classifiers import Classifier
 from ..utils import benchmark
 
 
-__all__ = ["compute", "predict", "process", "run", "ALGORITHMS"]
+__all__ = ["WekaClassifier", "CLASSIFIERS"]
 
-
-# list of classifiers to be used
-SELECTED = [
-    ("AB", "Adaptive Boosting"),
-    ("BN", 'Bayesian Network'),
-    ("BNB", "Bernoulli Naive Bayes"),
-    ("D", "Decorate"),
-    ("DT", "Decision Tree"),
-    ("GNB", "Gaussian Naive Bayes"),
-    ("J48", "Decision Tree"),
-    ("kNN", "k-Nearest Neighbors"),
-    ("LR", "Logistic Regression"),
-    ("LSVM", "Linear Support Vector Machine"),
-    ("MLP", "Multi-Layer Perceptron"),
-    ("MNB", "Multinomial Naive Bayes"),
-    ("RF", "Random Forest"),
-    ("SVM", "Support Vector Machine"),
-]
-
-ALGORITHMS = [abbr for abbr, _ in SELECTED]
 
 # list of implemneted classifiers
 CLASSIFIERS = {
@@ -190,115 +169,6 @@ CV_PARAM = {
 }
 
 
-@benchmark
-def compute(dataset, name, cv=3, n_jobs=4, logger=None):
-    """ Training method handling the cross-validation. """
-    try:
-        CLASSIFIERS[name]
-    except KeyError:
-        logger.debug("{} not available".format(name))
-        dataset.classifier = None
-        return
-    # get classifer and grid search parameters
-    try:
-        params = STATIC_PARAM[name]
-    except:
-        params = {}
-    try:
-        param_grid = CV_PARAM[name]
-    except:
-        param_grid = None
-    classifier = CLASSIFIERS[name](**params)
-    classifier.labels = dataset.labels
-    # if a param_grid is input, perform cross-validation and select the best classifier
-    logger.debug("Processing {}...".format(name))
-    if param_grid is not None:
-        logger.debug("> Applying Grid Search (CV={})...".format(cv))
-        grid = GridSearchCV(classifier, param_grid=param_grid, cv=cv,
-                            scoring='accuracy', n_jobs=n_jobs)
-        grid.fit(dataset.data, dataset.target)
-        logger.debug("> Best parameters found:\n  {}".format(grid.best_params_))
-        results = '\n'.join("  %0.3f (+/-%0.03f) for %r" % (m, s * 2, p) \
-            for m, s, p in zip(grid.cv_results_['mean_test_score'],
-                               grid.cv_results_['std_test_score'],
-                               grid.cv_results_['params']))
-        logger.debug(" > Grid scores:\n{}".format(results))
-        params.update(grid.best_params_)
-        classifier = CLASSIFIERS[name](**params)
-    # now fit the (best) classifier and predict labels
-    logger.debug("> Fitting the classifier...")
-    classifier.fit(dataset.train.data, dataset.train.target)
-    logger.debug("> Making predictions...")
-    dataset.test.prediction = classifier.predict(dataset.test.data)
-    dataset.test.proba = classifier.predict_proba(dataset.test.data)[:, 1]
-    dataset.classifier = classifier
-
-
-@benchmark
-def predict(classifier, data, **kwargs):
-    """ Prediction function. """
-    return classifier.predict(data)
-
-
-def process(algorithms, dataset, cv, n_jobs, logger=None):
-    """ Generator for processing each algorithm from a given list. """
-    # execute ML algorithms and collect metrics for display
-    for name, description in algorithms:
-        if isinstance(description, dict):
-            description = description.values()[0]
-        compute(dataset, name, cv, n_jobs, logger=logger, info=name)
-        if dataset.classifier is None:
-            continue
-        yield [name, description] + metrics(dataset, logger=logger)
-
-
-@benchmark
-def run(**kwargs):
-    """ Execute tests on the given dataset and classifiers. """
-    branch = kwargs.pop("branch")
-    logger = kwargs.get("logger")
-    # get rid of useless kwargs
-    kwargs.pop("_debug_level")
-    kwargs.pop("verbose")
-    # extract kwargs for the compute function
-    cv = kwargs.pop("cv", 3)
-    n_jobs = kwargs.pop("jobs", 4)
-    extra = kwargs.pop("extra", False)
-    algo = kwargs.pop("algorithm", None)
-    # process the dataset
-    only_dataset = kwargs.pop("dataset", False)
-    force = kwargs.pop("force", False) or only_dataset
-    dataset = Dataset(force=force, **kwargs)
-    if only_dataset:
-        print(dataset)
-        return
-    dataset.prepare()
-    if branch == "test":
-        c = kwargs.get("classes")
-        c = ["class {}", "classes {}"][len(c) > 1].format(','.join(c))
-        # process selected algorithms (from [CRESCI])
-        table_data = [["", "Model", "Accuracy", "Precision", "Recall", "F-Measure", "MCC", "AUC"]]
-        for row in process(SELECTED, dataset, cv, n_jobs, logger=logger):
-            table_data.append(row)
-        table = Table(highlight_best(table_data), "Results ([CRESCI] - {})".format(c))
-        print(table.table)
-        # process extra algorithms (not from [CRESCI])
-        if extra:
-            table_data = [["", "Model", "Accuracy", "Precision", "Recall", "F-Measure", "MCC", "AUC"]]
-            for row in process(EXTRA, dataset, cv, n_jobs, logger=logger):
-                table_data.append(row)
-            table = Table(highlight_best(table_data), "Results (others - {})".format(c))
-            print(table.table)
-    elif branch == "predict":
-        # train a selected classifier
-        compute(dataset, algo, cv, n_jobs, logger=logger, info=algo)
-        # load new data and predict new labels
-        ds = Dataset(**kwargs)
-        p = predict(dataset.classifier, ds.data, logger=logger, info=len(ds.data))
-        for i, new_label in enumerate(p):
-            logger.info("#{}: {}".format(i, ds.labels[new_label]))
-
-
 # ---------------------------------------------- WEKA ALGORITHM BINDINGS -----------------------------------------------
 class WekaClassifier(Classifier):
     """ This class implements a binding for using the Decorate algorithm from the Weka framework the same way as SkLearn
@@ -342,6 +212,27 @@ class WekaClassifier(Classifier):
             if target == prediction:
                 correct += 1
         return float(correct) / total
+    
+    @classmethod
+    def to_arff(cls, dataset):
+        """ This class method generates an ARFF file of the dataset, suitable for use with the Weka framework.
+        
+        NB: this method is suited to the Dataset abstraction of pbox.items.dataset """
+        ds = dataset
+        l = len(dataset._features)
+        ARFF_TEMPLATE = "@RELATION \"{rel}\"\n\n{attr}\n@ATTRIBUTE {c: <%s} {cls}\n\n@DATA\n{data}" % l
+        for f, dss in zip([cls.train_file, cls.test_file], [ds.train, ds.test]):
+            f = Path(f)
+            Path(f.dirname, create=True)
+            a = []
+            for name in ds._features.keys():
+                value = dataset._data[0][name]
+                attr_type = ["string", "numeric"][isinstance(value, int) or isinstance(value, float)]
+                a.append(("@ATTRIBUTE {: <%s} {}" % l).format(name, attr_type))
+            d = "\n".join(",".join(map(str, row)) + "," + tgt for row, tgt in zip(dss.data, dss.target))
+            with f.open('w') as arff:
+                arff.write(ARFF_TEMPLATE.format(rel=ds.name, attr="\n".join(a), c="class",
+                                                cls="{%s}" % ",".join(set(dss.target['label'].tolist())), data=d))
 
 
 class BN(WekaClassifier):
