715788caa6fca41db116aab2d60fe83f0de1103a
dhondta <alexandre.dhondt@gmail.com>
2021-11-02 16:08:37 +0100

    Added support for binary-class detection

--- src/lib/pbox/items/detector.py
+++ src/lib/pbox/items/detector.py
@@ -25,21 +25,27 @@ class Detector(Base):
         If called from an instance:
             Runs the detector according to its command line format and checks if the executable has been changed by this
              execution. """
+        l = kwargs.get('label')
+        actual_label = [(), (l if multiclass else l is not None, )]['label' in kwargs]
         if isinstance(self, type):
             registry = [d for d in Detector.registry if d.check(Executable(executable).category) and \
                                                         getattr(d, "vote", True)]
             results, details = {None: -len(registry) + kwargs.get('threshold', 3)}, {}
             for detector in registry:
+                # do not consider Yes|No-detectors if the multiclass option is set
                 if multiclass and not getattr(detector, "multiclass", True):
                     continue
                 label = list(detector.detect(executable, **kwargs))[0][1]
-                if label is False:
+                if label == -1 and getattr(detector, "multiclass", True):
                     continue
+                # if the multiclass option is unset, convert the result to Yes|No
+                if not multiclass and getattr(detector, "multiclass", True):
+                    label = label is not None
                 results.setdefault(label, 0)
                 results[label] += 1
                 details[detector.name] = label
             label = max(results, key=results.get)
-            r = (executable, label) + [(), (kwargs['label'], )]['label' in kwargs]
+            r = (executable, label) + actual_label
             if kwargs.get("debug", False):
                 r += (details, )
             return r
@@ -48,14 +54,16 @@ class Detector(Base):
             e = executable if isinstance(executable, Executable) else Executable(executable)
             if e.category not in self._categories_exp or \
                e.extension[1:] in getattr(self, "exclude", {}).get(e.category, []):
-                return False
+                return -1
             # now try to detect a packer on the input executable
             label = self.run(e, **kwargs)
             # if packer detection succeeded, we can return packer's label
             if label:
                 self.logger.debug("%s detected as packed with %s by %s" % (e.filename, label, self.name))
                 label = label.strip()
-            return (e, label if multiclass else label is not None) + [(), (kwargs['label'], )]['label' in kwargs]
+            if not multiclass:
+                label = label is not None if getattr(self, "multiclass", True) else label.lower() == "true"
+            return (e, label) + actual_label
     
     @file_or_folder_or_dataset
     def test(self, executable, multiclass=True, **kwargs):
