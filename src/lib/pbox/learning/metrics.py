6b1dbeb05453dacb9b599462785be7e92b024385
dhondta <alexandre.dhondt@gmail.com>
2023-01-07 20:39:04 +0100

    Improved clustering metrics

--- src/lib/pbox/learning/metrics.py
+++ src/lib/pbox/learning/metrics.py
@@ -26,8 +26,16 @@ METRIC_DISPLAY = {
         'MCC':       "%",
         'AUC':       "%",
     },
-    'clustering': {
+    'clustering_supervised': {
+        'Randomness Score': "nbr",
+        'Adjusted Mutual Information Score': "nbr",
+        'Homogeneity Completeness V-Measure': "nbr",
     },
+    'clustering_unsupervised': {
+        'Silhouette Score': "nbr",
+        'Calinski Harabasz Score': "nbr",
+        'Davies Bouldin Score': "nbr",
+    }
     'regression': {
         'MSE': "nbr",
         'MAE': "nbr",
@@ -148,17 +156,23 @@ def classification_metrics(y_pred, y_true=None, y_proba=None, labels=None, avera
 
 
 @_convert_output
-def clustering_metrics(X, y_pred, y_true=None, **kwargs):
+def clustering_supervised_metrics(y_pred, y_true=None, **kw):
     """ Compute clustering-related metrics, either based only on predicted values or both true and predicted values. """
     # get the true and predicted values without the not-labelled ones and as integers
     yt, yp, _ = _map_values_to_integers(y_true, y_pred, **kw)
-    silhouette = silhouette_score(X, y_pred, metric='euclidean')
-    calinski_harabasz = calinski_harabasz_score(X, y_pred)
-    davies_bouldin = davies_bouldin_score(X, y_pred)
-    rand_index = rand_score(y_true, y_pred)
-    mutual_information = adjusted_mutual_info_score(y_true, y_pred)  
-    homogeneity, completeness, v_measure = homogeneity_completeness_v_measure(y_true, y_pred)
-    return [silhouette, calinski_harabasz, davies_bouldin], metric_headers("clustering", **kw)
+    return [rand_score(yt, yp), adjusted_mutual_info_score(yt, yp), omogeneity_completeness_v_measure(yt, yp)], \
+           metric_headers("clustering_supervised", **kw)
+
+
+@_convert_output
+def clustering_unsupervised_metrics(y_pred, y_true=None, **kw):
+    """ Compute clustering-related metrics, either based only on predicted values or both true and predicted values. """
+    # get the true and predicted values without the not-labelled ones and as integers
+    #FIXME: include data (X) in the function's signature
+    X = kw['X']
+    yt, yp, _ = _map_values_to_integers(y_true, y_pred, **kw)
+    return [silhouette_score(X, yp, metric="euclidean"), calinski_harabasz_score(X, yp), davies_bouldin_score(X, yp)], \
+           metric_headers("clustering_unsupervised", **kw)
 
 
 @_convert_output
