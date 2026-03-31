8178c79ef1d588969dbd55d3d45487e63e3a715a
babou7063 <103448627+babou7063@users.noreply.github.com>
2026-03-31 16:36:17 +0200

    First commit init creation

---
+++ src/lib/src/pboxllm/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: UTF-8 -*-
+from .backend import LLMBackend
+from .formatter import FeatureFormatter
+from .strategy import PromptStrategy
+
+__all__ = ["LLMBackend", "FeatureFormatter", "PromptStrategy"]
